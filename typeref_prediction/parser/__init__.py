96a911fe979f3bdc43a065de56fb522366466aa1
Wei Ma <weima@WeitekiMacBook-Pro.local>
2023-06-23 15:07:20 +0800

    remove old

--- typeref_prediction/parser/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import (remove_comments_and_docstrings,
-                   tree_to_token_index,tree_to_token_index_with_type,index_to_code_token_types,
-                   index_to_code_token,index_to_code_token_variables,
-                   tree_to_variable_index)
-from .DFG import DFG_python,DFG_java,DFG_ruby,DFG_go,DFG_php,DFG_javascript,DFG_csharp
